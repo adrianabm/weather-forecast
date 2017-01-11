592348f741c12f3c4d2667e9fb43bd4121ef666f
Adriana Matsufuji <adrianabm@gmail.com>
2017-01-11 11:58:59 +0100

    WeatherList rendering search term

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class WeatherList extends Component {
+
+  renderWeather(cityData) {
+    return (
+      <tr key={ cityData.city.id }>
+        <td>{ cityData.city.name }, { cityData.city.country }</td>
+      </tr>
+    )
+  }
+
   render() {
     return (
       <table className="table table-hover">
         <tbody>
-          <tr>
-            <td>City</td>
-          </tr>
+          { this.props.weather.map(this.renderWeather) }
         </tbody>
       </table>
     )
