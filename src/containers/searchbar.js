b9af616803b50ca1afb5f98b8ba400325a3cd563
Adriana Matsufuji <adrianabm@gmail.com>
2017-01-11 10:23:07 +0100

    Searchbar as a controlled component

--- src/containers/searchbar.js
+++ src/containers/searchbar.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { term: '' }
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value)
+    this.setState({ term: event.target.value })
+  }
+
   render() {
     return (
       <form className="search-input">
-        <input />
+        <input
+          placeholder="Find your city..."
+          className="form-control"
+          value={ this.state.term }
+          onChange={ this.onInputChange.bind(this) }
+        />
         <span className="search-btn">
           <button type="submit" className="btn btn-secondary">Search</button>
         </span>
